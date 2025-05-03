2dca73bb03fdc7b6f0490ecd1328d33d14fa49d6
龚文硕 <gongwenshuo@chaboshi.cn>
2025-05-03 22:23:27 +0800

    重新加注释

--- test/choo_window_manager_test.dart
+++ test/choo_window_manager_test.dart
@@ -1,19 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:choo_window_manager/choo_window_manager.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockChooWindowManagerPlatform
-    with MockPlatformInterfaceMixin
-    {
-
+class MockChooWindowManagerPlatform with MockPlatformInterfaceMixin {
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 }
 
 void main() {
-
-
   test('getPlatformVersion', () async {
-    MockChooWindowManagerPlatform fakePlatform = MockChooWindowManagerPlatform();
+    MockChooWindowManagerPlatform fakePlatform =
+        MockChooWindowManagerPlatform();
   });
 }
